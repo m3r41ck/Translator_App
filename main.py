58623c196a0b57b5f9efaafcfa4a6b4bdfad21b5
Merrick <mrcalder19@gmail.com>
2024-06-10 10:43:03 -0700

    Reformatted form HTML and added functionality

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods=('GET', 'POST'))
+def translate():
+    if request.method == 'POST':
+        input_translation = request.form['inputText']
+        print(input_translation)
+        return render_template('index.html')
+
     return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
